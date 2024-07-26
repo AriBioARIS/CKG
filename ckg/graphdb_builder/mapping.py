1d0e9cdc963e5b937092c587fc11febc424c60ac
choung0124 <hschoung0124@gmail.com>
2024-07-26 11:15:45 +0900

    Custom

--- ckg/graphdb_builder/mapping.py
+++ ckg/graphdb_builder/mapping.py
@@ -96,7 +96,7 @@ def getMappingForEntity(entity):
     if entity in dbconfig["sources"]:
         mapping_file = os.path.join(ckg_config["databases_directory"], os.path.join(dbconfig["sources"][entity], "complete_mapping.tsv"))
         max_wait = 0
-        while not os.path.isfile(mapping_file) and max_wait < 5000:
+        while not os.path.isfile(mapping_file) and max_wait < 64:
             time.sleep(15)
             max_wait += 1
 
